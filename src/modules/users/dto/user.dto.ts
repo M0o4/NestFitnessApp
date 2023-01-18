569a83b0da37331003609274a822835bbb372cda
Vladislav Indrey <v.indrej@nv.dunice.net>
2023-01-18 21:59:40 +0300

    Create validator pipe

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,10 +1,23 @@
+import { IsNotEmpty, MinLength } from 'class-validator';
+
 export class UserDTO {
   readonly username: string;
+
+  @IsNotEmpty()
+  @MinLength(3)
   readonly login: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
   readonly password: string;
+
   readonly sex: number;
+
   readonly height: number;
+
   readonly weight: number;
+
   readonly birthDate: Date;
+
   readonly role: number;
 }
