ab336d7301f93b8a0ea343559368dadd9fc4566e
Vladislav Indrey <v.indrej@nv.dunice.net>
2023-01-18 21:23:21 +0300

    Create auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+
+import { UsersModule } from '../users/users.module';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [PassportModule, UsersModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
+  async validateUser(login: string, pass: string) {
+    const user = await this.userService.findOneByLogin(login);
+    if (!user) {
+      return null;
+    }
+
+    const match = await this.comparePassword(pass, user.password);
+    if (!match) {
+      return null;
+    }
+
+    const { password, ...result } = user['dataValues'];
+    return result;
+  }
+
+  private async comparePassword(enteredPassword, dbPassword) {
+    const match = await bcrypt.compare(enteredPassword, dbPassword);
+    return match;
+  }
+}
